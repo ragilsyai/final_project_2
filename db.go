b1adad80d7037e832f9878994f53b8fbe4d4b32b
ragilsyai <111896286+ragilsyai@users.noreply.github.com>
2022-11-02 20:06:43 +0700

    Add files via upload

---
+++ db.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"FP2/models"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+)
+
+func DBinit(dbHost, dbPort, dbUsername, dbPassword, dbName string) *gorm.DB {
+	dsn := fmt.Sprintf("%s:@tcp(%s:%s)/%s?charset=utf8&parseTime=True", dbUsername, dbHost, dbPort, dbName)
+
+	fmt.Println(dsn)
+	db, err := gorm.Open("mysql", dsn)
+	if err != nil {
+		log.Printf("ERROR: Failed to connect to Database -> %v\n", err)
+	}
+	db.AutoMigrate(models.User{}, models.Photo{}, models.Comment{}, models.SocialMedia{})
+	return db
+}
