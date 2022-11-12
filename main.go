40a6803755d63ce64b6478af9c7da273cf8f2497
Al Bukhari Bin Riedho <104047361+bukhari0281@users.noreply.github.com>
2022-11-12 17:40:16 +0700

    Update main.go

--- main.go
+++ main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"FP2/router"
 	"os"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	r := router.StartAPP()
-	PORT := os.Getenv("MYSQLPORT")
-	r.Run(":" + PORT)
+	PORT := os.Getenv("SERVICE_PORT")
+	if PORT == "" {
+		PORT = "9000"
+	}
+	err := r.Run()
+	if err != nil {
+		log.Println("Error Graceful", err)
+		os.Exit(1)
+	}
 }
